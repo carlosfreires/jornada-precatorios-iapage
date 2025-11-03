4cc91a8eb3680563fac0f770a664111bb560244e
Carlos Freires <freires.carlos@outlook.com>
2025-11-03 04:10:51 -0300

    Auto-generated commit

--- src/utils/format-currency.ts
+++
@@ -1,4 +0,0 @@
-// utilitário para formatar moeda BRL
-export function FormatCurrency(value: number) {
-  return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
-}
